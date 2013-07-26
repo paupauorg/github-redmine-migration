f3d0eac18ae31a5cc4e240056abb35f5eb5f3c38
poloniculmov <github@poloniculmov.com>
2013-07-26 14:07:10 +0300

    Set width to 50% fixes #34

--- import.rb
+++ import.rb
@@ -201,7 +201,7 @@ end
 
 
 def clean_pandoc(block)
-  block.gsub('"$":', '').gsub('!http', '!{width: 100%}http')
+  block.gsub('"$":', '').gsub('!http', '!{width: 50%}http')
 end
 
 def find_or_create_user(github_login)
